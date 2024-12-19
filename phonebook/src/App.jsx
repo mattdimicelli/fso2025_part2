feac5e0c3f5cc5da55b50ef3fc8b0264b5f0c974
Matthew Di Micelli <matthewdimicelli@gmail.com>
2024-12-18 20:46:29 -0500

    - changes for 3.19

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Directory from './components/Directory.jsx'
+import Notification from './components/Notification.jsx'
 import Form from './components/Form.jsx'
 import httpService from './services/httpService.js';
 const { getDirectory, postEntry } = httpService;
@@ -8,6 +9,7 @@ const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [notification, setNotification] = useState('');
 
   useEffect(() => {
     getDirectory().then(persons => setPersons(persons));
@@ -22,6 +24,10 @@ const App = () => {
         } else {
           setPersons(persons.filter(person => person.name !== newName).concat(entry.entry))
         }
+      })
+      .catch(e => {
+        setNotification(e.response.data.error);
+        setTimeout(() => setNotification(''), 3000);
     });
     setNewName('')
     setNewNumber('')
@@ -38,6 +44,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notification} />
       <Form handleChangeNumber={handleChangeNumber} handleSubmit={handleSubmit} handleChangeName={handleChangeName}
             newName={newName} newNumber={newNumber}
       />
---
+++ phonebook/src/components/Notification.jsx
@@ -0,0 +1,7 @@
+const Notification = ({ message }) => {
+  return (
+    <div style={{ color: 'red' }}>{ message }</div>
+  )
+}
+
+export default Notification;
